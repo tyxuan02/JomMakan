64588d1551e1a6e8564f03b49a7a22ba09c4b8b1
Teea Yu Xuan <yuxuanteea02@gmail.com>
2022-12-30 08:08:30 +0800

    - Allow user to change food quantity in food page - Disable the functionalities of changing food quantity and adding food to cart when the food is not available (the stall is close)

--- app/src/main/java/com/example/jommakan/FoodFragment.java
+++ app/src/main/java/com/example/jommakan/FoodFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +30,12 @@ public class FoodFragment extends Fragment {
     Date currentTime, open, close;
     View shadow_background;
     TextView not_available_text;
+    ImageButton decrement_button, increment_button;
+    TextView quantity;
+    Button add_to_cart_button;
+    TextView quantity_text;
+
+    int count = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,6 +54,11 @@ public class FoodFragment extends Fragment {
         food_description = view.findViewById(R.id.food_description);
         shadow_background = view.findViewById(R.id.shadow_background);
         not_available_text = view.findViewById(R.id.not_available_text);
+        decrement_button = view.findViewById(R.id.decrement_button);
+        increment_button = view.findViewById(R.id.increment_button);
+        quantity = view.findViewById(R.id.quantity);
+        add_to_cart_button = view.findViewById(R.id.add_to_cart_button);
+        quantity_text = view.findViewById(R.id.quantity_text);
 
         // Hide top navigation bar
         Toolbar toolbar = ((MainActivity) getActivity()).findViewById(R.id.toolbar);
@@ -77,10 +89,53 @@ public class FoodFragment extends Fragment {
         if (currentTime.after(open) && currentTime.before(close)) {
             shadow_background.setVisibility(View.INVISIBLE);
             not_available_text.setVisibility(View.INVISIBLE);
+
+            // Display increment, decrement and add to cart buttons
+            increment_button.setVisibility(View.VISIBLE);
+            decrement_button.setVisibility(View.VISIBLE);
+            add_to_cart_button.setVisibility(View.VISIBLE);
+            quantity.setVisibility(View.VISIBLE);
+            quantity_text.setVisibility(View.VISIBLE);
+            quantity.setText("1");
         } else {
             shadow_background.setVisibility(View.VISIBLE);
             not_available_text.setVisibility(View.VISIBLE);
+
+            // Hide increment, decrement and add to cart buttons
+            increment_button.setVisibility(View.INVISIBLE);
+            decrement_button.setVisibility(View.INVISIBLE);
+            add_to_cart_button.setVisibility(View.INVISIBLE);
+            quantity.setVisibility(View.INVISIBLE);
+            quantity_text.setVisibility(View.INVISIBLE);
         }
+
+        // Increment, decrement and add to cart buttons
+        increment_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                quantity.setText(String.valueOf(count));
+            }
+        });
+
+        decrement_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (count <= 0) {
+                    count = 0;
+                } else {
+                    count--;
+                }
+                quantity.setText(String.valueOf(count));
+            }
+        });
+
+        add_to_cart_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Add to cart after user clicks it
+            }
+        });
     }
 
     private void checkOpenClose(String openTime, String closeTime) {
